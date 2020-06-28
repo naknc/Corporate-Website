f8645787d6a5a3c415314cbc64318ae990090dc1
naknc <nihan.akinci.35@gmail.com>
2020-06-28 23:12:15 +0300

    About and Contact

---
+++ KurumsalWeb/Controllers/AboutUsController.cs
@@ -0,0 +1,44 @@
+﻿using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        CorporateDBContext db = new CorporateDBContext();
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            var h = db.AboutUs.ToList();
+            return View(h);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var h = db.AboutUs.Where(x => x.AboutUsId == id).FirstOrDefault();
+            return View(h);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(int id, AboutUs a)
+        {
+            if (ModelState.IsValid)
+            {
+                var aboutus = db.AboutUs.Where(x => x.AboutUsId == id).SingleOrDefault();
+
+                aboutus.Description = a.Description;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(a);
+        }
+    }
+}
